2eef1c1bc8984585009fc34e214265d776361ece
Alvino0706 <alvino0706@gmail.com>
2025-09-17 14:17:38 +0700

    Tutorial 3

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alvino-revaldi-footballnews.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
-    "<https://alvino-revaldi-footballnews.pbp.cs.ui.ac.id.>"
+    "https://alvino-revaldi-footballnews.pbp.cs.ui.ac.id"
 ]
 
 
